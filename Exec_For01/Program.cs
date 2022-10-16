097e53a03b97fa3d01a089acdc3a5b37136f4775
yiru00 <eva890706@gmail.com>
2022-10-16 22:59:43 +0800

    “作業 git log git status d git commit -m exec_for01

---
+++ Exec_For01/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //倒三角形
+
+        for (int i = 5; i > 0; i--)
+        {
+            string row = new string('*', i);
+            Console.WriteLine(row);
+        }
+    }
+}
